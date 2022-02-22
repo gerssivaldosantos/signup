a37e61ac935899b7028da48e5792147fad77ef9b
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-22 14:44:57 -0300

    refactor: replacing the default by basic

--- signup/src/pages/Index.vue
+++ signup/src/pages/Index.vue
@@ -1,49 +1,30 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <div class="main">
+    <div class="title">Root</div>
+  </div>
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models'
-import ExampleComponent from 'components/CompositionComponent.vue'
-import { defineComponent, ref } from 'vue'
-
-export default defineComponent({
-  name: 'PageIndex',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ])
-    const meta = ref<Meta>({
-      totalCount: 1200
-    })
-    return { todos, meta }
-  }
-})
+<script>
+export default {
+  name: 'Index'
+}
 </script>
+
+<style>
+.main {
+  background-color: rgb(32, 32, 32);
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  flex: 1;
+  display: flex;
+  height: 100vh;
+}
+
+.title {
+  font-size: 4em;
+  color: #42b883;
+  padding-bottom: 150px;
+}
+</style>
