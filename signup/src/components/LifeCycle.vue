276944cde000328588738c63fa5516686115634e
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-23 08:31:53 -0300

    feat: life cycle component

---
+++ signup/src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>The data of app is {{ test }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LifeCycle',
+  data () {
+    return {
+      test: 'Life Cycle'
+    }
+  },
+  created () {
+    setTimeout(() => {
+      this.test = 'Life Cycle 2'
+    }, 2000)
+  },
+
+  mounted () {
+    this.test = 'Life Cycle mounted'
+  }
+}
+
+</script>
+
+<style></style>
--- signup/src/pages/Login.vue
+++ signup/src/pages/Login.vue
@@ -3,19 +3,21 @@
     <h1>Hellow Vue</h1>
     <h4>My name is {{ name }}</h4>
     <h4>I work with {{ job }}</h4>
+    <LifeCycle />
   </div>
 </template>
 
 <script>
+import LifeCycle from 'src/components/LifeCycle.vue'
 export default {
   name: 'Login',
-  // eslint-disable-next-line space-before-function-paren
-  data() {
+  data () {
     return {
       name: 'Gerssivaldo',
       job: 'Development'
     }
-  }
+  },
+  components: { LifeCycle }
 }
 </script>
 
