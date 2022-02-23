493645fcf16d8298de32da71e694671e7e56c604
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-02-23 14:53:54 -0300

    feat: request helper

---
+++ signup/src/helper/requestHelper.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosPromise } from 'axios'
+
+const baseUrl = process.env.API || ''
+
+const getHeaders = (headers = {}) => {
+  if (localStorage.token) {
+    const token = localStorage.getItem('token')
+    return {
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...headers
+    }
+  }
+}
+export const useRequest = {
+  get (routeName:string, headers = {}) {
+    return axios.get(`${baseUrl}/${routeName}`, { headers: getHeaders(headers) })
+  },
+  post (routeName:string, params?:unknown, headers = {}):AxiosPromise {
+    return axios.post(`${baseUrl}/${routeName}`, params, { headers: getHeaders(headers) })
+  }
+
+}
