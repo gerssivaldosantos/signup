04c3adad8090d07a7428f55da92fd67d9ef917d5
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-03-07 17:06:23 -0300

    feat: new properties

--- signup/src/@types/userRequest.ts
+++ signup/src/@types/userRequest.ts
@@ -1,5 +1,9 @@
-export interface userRequest {
-  data: {
-    token: string;
-  }
+export interface userRequest{
+    createdAt: string;
+    email: string;
+    emailToken: string;
+    id: string;
+    isValidated: boolean;
+    name: string;
+    updatedAt: string
 }
