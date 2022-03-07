b21e523dedd3111ce5cae8c325f47813e28277c8
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-03-07 16:22:39 -0300

    feat: new request type

--- signup/src/helper/requestHelper.ts
+++ signup/src/helper/requestHelper.ts
@@ -1,10 +1,15 @@
 import axios, { AxiosPromise } from 'axios'
+import { userRequest } from 'src/@types/userRequest'
 
 const baseUrl = 'http://localhost:8085'
 
 const getHeaders = (headers = {}) => {
   if (localStorage.token) {
-    const token = localStorage.getItem('token')
+    const userInfo = <userRequest>(
+      JSON.parse(localStorage.getItem('userInfo') || '')
+    )
+    const token = userInfo.data.token
+    console.log(token)
     return {
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
       ...headers
@@ -14,13 +19,21 @@ const getHeaders = (headers = {}) => {
 
 export const useUserRequest = {
   get (routeName: string, headers = {}) {
-    return axios.get(`${baseUrl}/${routeName}`, { headers: getHeaders(headers) })
+    return axios.get(`${baseUrl}/${routeName}`, {
+      headers: getHeaders(headers)
+    })
   }
 }
 
 export const useAuthRequest = {
-  async post (routeName: string, params?: unknown, headers = {}): Promise<AxiosPromise> {
-    const result = await axios.post(`${baseUrl}/${routeName}`, params, { headers: getHeaders(headers) })
+  async post (
+    routeName: string,
+    params?: unknown,
+    headers = {}
+  ): Promise<AxiosPromise> {
+    const result = await axios.post(`${baseUrl}/${routeName}`, params, {
+      headers: getHeaders(headers)
+    })
     return result
   }
 }
