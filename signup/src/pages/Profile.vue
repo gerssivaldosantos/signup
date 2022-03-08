0a0fa9c9bafd8c1e36c0bc77d400578b0600eac3
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-03-08 00:07:07 -0300

    feat: removing the directives

--- signup/src/pages/Profile.vue
+++ signup/src/pages/Profile.vue
@@ -3,7 +3,7 @@
     <q-page>
       <q-bar style="display: flex; justify-content: space-between;" >
         <q-icon name="person" />
-        <h2 class="personName">{{personName}}</h2>
+        <h2 class="personName">{{userInfo?.name}}</h2>
         <q-btn @click="onLogout" color="black" label="logout"/>
       </q-bar>
     </q-page>
@@ -13,9 +13,9 @@
 <script setup lang="ts" >
 import { useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
-import { useUserRequest } from 'src/helper/requestHelper'
+import { userRequest } from 'src/@types/userRequest'
 
-const personName = ref<string>('Loading ...')
+const userInfo = ref<userRequest>()
 const router = useRouter()
 
 const onLogout = async () => {
@@ -27,20 +27,10 @@ const onLogout = async () => {
   }
 }
 
-const getPersonInfo = async () => {
-  try {
-    const result = await useUserRequest.get('user')
-    return result
-  } catch (err) {
-    alert(err)
-  }
-}
 onMounted(() => {
   if (!localStorage.getItem('userInfo')) {
     void router.push('/login')
   }
-  const user = getPersonInfo()
-  console.log(user)
 })
 
 </script>
