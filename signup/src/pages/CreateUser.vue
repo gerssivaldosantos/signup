ff54e2e657374c05551fbf140e0599ba0326ec03
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-03-08 00:17:10 -0300

    feat: create page for users

---
+++ signup/src/pages/CreateUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="bg-blue-10 window-height window-width row justify-center items-center">
+    <div class="column">
+      <div class="row">
+        <h5 class="text-h5 text-white q-my-md">Andows's Market Place</h5>
+      </div>
+      <div class="row">
+        <q-card style="padding: 100px; " square bordered class="q-pa-lg shadow-5">
+          <q-card-section>
+            <q-form class="q-gutter-md"></q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn unelevated color="primary" size="lg" class="full-width" label="Login" />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-none">
+            <p class="text-grey-6"></p>
+            <q-btn outline color="primary" label="Create an Account" />
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
