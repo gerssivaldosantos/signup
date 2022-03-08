73805ddb6e2b3647d74856a729fc2d12ec3c6336
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-03-08 08:55:15 -0300

    feat: inputs and buttons created

--- signup/src/pages/CreateUser.vue
+++ signup/src/pages/CreateUser.vue
@@ -2,19 +2,51 @@
   <q-page class="bg-blue-10 window-height window-width row justify-center items-center">
     <div class="column">
       <div class="row">
-        <h5 class="text-h5 text-white q-my-md">Andows's Market Place</h5>
+        <div class="row">
+          <q-btn @click="router.push('/login')" flat color="white" icon="arrow_back" />
+          <p class="text-h5 text-white q-my-md">New User</p>
+        </div>
       </div>
       <div class="row">
-        <q-card style="padding: 100px; " square bordered class="q-pa-lg shadow-5">
+        <q-card
+          style="padding: 50px; max-width: 800px; min-width: 500px; "
+          square
+          bordered
+          class="q-pa-lg shadow-5"
+        >
           <q-card-section>
             <q-form class="q-gutter-md"></q-form>
+            <q-input outlined v-model="name" type="text" label="Name" />
+            <br />
+            <q-input outlined v-model="email" type="text" label="Email" />
+            <br />
+            <q-input
+              outlined
+              :type="isPwd ? 'password' : 'text'"
+              v-model="password"
+              label="password"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
           </q-card-section>
           <q-card-actions class="q-px-md">
-            <q-btn unelevated color="primary" size="lg" class="full-width" label="Login" />
+            <q-btn
+              @click="onCreateUser"
+              unelevated
+              color="primary"
+              size="lg"
+              class="full-width"
+              label="Create"
+            />
           </q-card-actions>
           <q-card-section class="text-center q-pa-none">
             <p class="text-grey-6"></p>
-            <q-btn outline color="primary" label="Create an Account" />
           </q-card-section>
         </q-card>
       </div>
@@ -24,4 +56,35 @@
 
 <script setup lang="ts">
 
+import { useAuthRequest } from 'src/helper/requestHelper'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { Notify } from 'quasar'
+const isPwd = ref<boolean>(true)
+const name = ref<string>()
+const email = ref<string>()
+const password = ref<string>()
+const userTypeId = '0e86ecd4-ad5f-4321-98bf-c6892cc48814'
+const router = useRouter()
+
+const onCreateUser = async () => {
+  try {
+    await useAuthRequest.post('users', {
+      name: name.value,
+      email: email.value,
+      password: password.value,
+      user_type_id: userTypeId
+    })
+    Notify.create({
+      message: 'Created',
+      color: 'green'
+    })
+    await router.push('/login')
+  } catch (err) {
+    Notify.create({
+      message: 'Cannot create',
+      color: 'red'
+    })
+  }
+}
 </script>
